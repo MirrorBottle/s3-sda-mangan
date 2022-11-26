6db8b97ef6a92195f6d810e6ff1b7fa0a950893a
MirrorBottle <setiawanbayu66152@gmail.com>
2022-11-26 13:41:51 +0800

    fix: core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -23,51 +23,45 @@ int main() {
   bool is_login = false;
   int landing_choice, login_choice , main_choice, wbp_list_choice;
   bool is_login_running = false;
+  
+  while(is_running) {
 
+    landing_choice = menu::landing();
+    switch (landing_choice) {
+      case 1:
+        is_login = auth_controller::login();
+        if(is_login) {
+          utility::notify("success", "Berhasil login!");
 
-  auth.id = "1";
-  auth.name = "Bayu Setiawan";
-  auth.role = "admin";
-
-  admin::index();
-  // while(is_running) {
-
-  //   landing_choice = menu::landing();
-  //   switch (landing_choice) {
-  //     case 1:
-  //       is_login = auth_controller::login();
-  //       if(is_login) {
-  //         utility::notify("success", "Berhasil login!");
-
-  //         if(auth.role == "admin") {
-  //           admin::index();
-  //         } else if (auth.role == "driver") {
-  //           driver::index();
-  //         } else {
-  //           user::index();
-  //         }
-  //       } else {
-  //         utility::notify("error", "Akun tidak terdaftar!");
-  //       }
-  //       break;
-  //     case 2:
-  //       auth_controller::regist();
-  //       user::index();
-  //       break;
-  //     case 4:
-  //       utility::header("Mangan - Tentang");
-  //       cout << "Mangan adalah simulasi pesan makanan sebuah restoran";
-  //       utility::notify("info", "Untuk Kembali", true);
-  //       break;
-  //     case 5:
-  //       is_running = false;
-  //       utility::notify("info", "Terima kasih!");
-  //     default:
-  //       if(landing_choice != 5) {
-  //         utility::notify("error", "Pilihan tidak ada!");
-  //       }
-  //       break;
-  //   }
-  // }
-  // return 0;
+          if(auth.role == "admin") {
+            admin::index();
+          } else if (auth.role == "driver") {
+            driver::index();
+          } else {
+            user::index();
+          }
+        } else {
+          utility::notify("error", "Akun tidak terdaftar!");
+        }
+        break;
+      case 2:
+        auth_controller::regist();
+        user::index();
+        break;
+      case 4:
+        utility::header("Mangan - Tentang");
+        cout << "Mangan adalah simulasi pesan makanan sebuah restoran";
+        utility::notify("info", "Untuk Kembali", true);
+        break;
+      case 5:
+        is_running = false;
+        utility::notify("info", "Terima kasih!");
+      default:
+        if(landing_choice != 5) {
+          utility::notify("error", "Pilihan tidak ada!");
+        }
+        break;
+    }
+  }
+  return 0;
 }
